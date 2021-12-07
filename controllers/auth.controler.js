9e76f56680ab99c3dc1d46f0c224725925320ed2
Wojciech Jarosz <jaroszw@gmail.com>
2021-12-07 20:46:27 +0100

    server borderplate set up

--- controllers/auth.controler.js
+++ controllers/auth.controler.js
@@ -4,12 +4,14 @@ const _ = require('lodash');
 const { OAuth2Client } = require('google-auth-library');
 const fetch = require('node-fetch');
 
-const { validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const expressJWT = require('express-jwt');
 
 // Mailer client
 const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.MAIL_KEY);
 
 //Custom error handler to get useful error from database errors
 const { errorHandler } = require('../helpers/dbErrorHandling');
@@ -17,5 +19,45 @@ const { errorHandler } = require('../helpers/dbErrorHandling');
 exports.registerController = async (req, res) => {
   const { name, email, password } = req.body;
 
-  res.json({ name, email, password });
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map((error) => error.msg)[0];
+    return res.status(200).json({
+      error: firstError,
+    });
+  } else {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        error: 'Email is taken',
+      });
+    }
+  }
+
+  const token = jwt.sign(
+    {
+      name,
+      email,
+      password,
+    },
+    process.env.JWT_ACCOUNT_ACTIVATION,
+    {
+      expiresIn: '15m',
+    }
+  );
+
+  const emailData = {
+    from: process.env.EMAIL_FROM,
+    to: to,
+    subject: 'Account activation link',
+    html: `
+      <h1>Pleace click to link to activate</h1>
+      <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
+      <hr/>
+      <p>This email contain sensitive info</p>
+      <p>${process.env.CLIENT_URL}</p>
+    `,
+  };
 };
