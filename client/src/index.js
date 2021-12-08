b3389bab8fd66afc5cdf500007bc5ec8ed56195e
Wojciech Jarosz <jaroszw@gmail.com>
2021-12-08 13:47:44 +0100

    register component layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
 
 import App from "./App";
+import Register from "./screens/Register";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/register" exact element={<Register />} />;
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
