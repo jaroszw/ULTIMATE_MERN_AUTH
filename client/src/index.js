7bae47634ced481537e85cbdae5f950140286357
Wojciech Jarosz <jaroszw@gmail.com>
2021-12-09 15:56:23 +0100

    login component wirh redirect

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
 
 import App from "./App";
 import Activate from "./screens/Activate";
+import Login from "./screens/Login";
 import Register from "./screens/Register";
 
 ReactDOM.render(
@@ -12,6 +13,7 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/register" exact element={<Register />} />;
+      <Route path="/login" exact element={<Login />} />;
       <Route path="/user/activation/:token" exact element={<Activate />} />
     </Routes>
   </BrowserRouter>,
